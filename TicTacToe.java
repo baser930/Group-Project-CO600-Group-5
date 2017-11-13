83ebfa16cd10a3f332a0033447111ddb83b75258
baser930 <32512478+baser930@users.noreply.github.com>
2017-11-13 12:26:18 +0000

    baz's game

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,40 +1,129 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author baser930 (Basiru Andrew)
+ * 
+ *  code based on online resources
+ *
  */
-package tictactoe;
+public class TicTacToe {
+	static Scanner in;
+	static String[] board;
+	static String turn;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+	public static void main(String[] args) {
+		in = new Scanner(System.in);
+		board = new String[9];
+		turn = "X";
+		String winner = null;
+		populateEmptyBoard();
 
+		System.out.println("Machine Learning X/O simple game");
+		System.out.println("--------------------------------");
+		printBoard();
+		System.out.println("X will always play first. To play insert your move type the wanted number :");
 
-public class TicTacToe implements ActionListener{
-    //Winning combinations
-    private int[][] winCombo = new int[][] {
-        {0, 1, 2},{3, 4, 5},{6, 7, 8}, //horizontal win
-        {0, 3, 6},{1, 4, 7},{2, 5, 8}, //vertical win
-        {0, 4, 8},{2, 4, 6} //diagonal win
-    };
-    //Whose Turn it is
-    private int count = 0;
-    private String letter = "";
-    
-    public void actionPerformed(ActionEvent a){
-    count++;
-    
-    if(count % 2 == 0){
-        letter = "O";
-    } else {
-        letter = "X";
-    }
-    
-    //Who won
-    }
+		while (winner == null) {
+			int numInput;
+			try {
+				numInput = in.nextInt();
+				if (!(numInput > 0 && numInput <= 9)) {
+					System.out.println("Invalid input; re-enter slot number:");
+					continue;
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid input; re-enter slot number:");
+				continue;
+			}
+			if (board[numInput-1].equals(String.valueOf(numInput))) {
+				board[numInput-1] = turn;
+				if (turn.equals("X")) {
+					turn = "O";
+				} else {
+					turn = "X";
+				}
+				printBoard();
+				winner = checkWinner();
+			} else {
+				System.out.println("Slot already taken; enter a different slot number:");
+				continue;
+			}
+		}
+		if (winner.equalsIgnoreCase("draw")) {
+			System.out.println("It's a draw!");
+		} else {
+			System.out.println("Congratulations! " + winner + "'s have won!");
+		}
+	}
+/*
+ * this method check all the 8 winning combinations with a switch
+ */
+	static String checkWinner() {
+		for (int a = 0; a < 8; a++) {
+			String line = null;
+			switch (a) {
+			case 0:
+				line = board[0] + board[1] + board[2]; // on the board this would be [1 - 2 - 3]
+				break;
+			case 1:
+				line = board[3] + board[4] + board[5]; 
+				break;
+			case 2:
+				line = board[6] + board[7] + board[8];
+				break;
+			case 3:
+				line = board[0] + board[3] + board[6];
+				break;
+			case 4:
+				line = board[1] + board[4] + board[7];
+				break;
+			case 5:
+				line = board[2] + board[5] + board[8];
+				break;
+			case 6:
+				line = board[0] + board[4] + board[8];
+				break;
+			case 7:
+				line = board[2] + board[4] + board[6]; //on the board this would be [3 - 5 - 7]
+				break;
+			}
+			// check if winner is X or O
+			if (line.equals("XXX")) {
+				return "X";
+			} else if (line.equals("OOO")) {
+				return "O";
+			}
+		}
 
-    
-    public static void main(String[] args) {
-    }
-    
+		for (int a = 0; a < 9; a++) {
+			if (Arrays.asList(board).contains(String.valueOf(a+1))) {
+				break;
+			}
+			else if (a == 8) return "draw";
+		}
+
+		System.out.println(turn + "'s turn; enter a slot number to place " + turn + " in:");
+		return null;
+	}
+/*
+ * This method is temporary, in future it will call Met's GUI class 
+ */
+	static void printBoard() {
+		System.out.println("/---|---|---\\");
+		System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+		System.out.println("/---|---|---\\"); //can't use \ it just looks weird 
+	}
+//clear and populate board
+	static void populateEmptyBoard() {
+		for (int a = 0; a < 9; a++) {
+			board[a] = String.valueOf(a+1);
+		}
+	}
 }
