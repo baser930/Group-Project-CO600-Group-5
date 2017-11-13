c0af88cba74b1ace3f5b533e63206b05890b15d2
JamesHannigan <james.hannigan@outlook.com>
2017-11-13 14:57:06 +0000

    Tahmoor's player class

--- src/Player.java
+++ src/Player.java
@@ -1,21 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 
 /**
- *
- * @author Pac
+ * 
  */
-public class Player {
-    void notifyWin(int h){}
-    void notifylLose(int h){}
-    void notifyDraw(int h){}
-    void aiMove(int ai, int Move){}
-    void humanMove(int h, int Move){}
-    void aiMark(int ai, int Mark){}
-    void humanMark(int h, int Mark){} 
+public class Player
+{
+    private String name;
+    private String noughtOrCross;
+    private int wins;
+
+    /**
+     * Constructor for objects of class Player
+     */
+    public Player(String name, String noughtOrCross)
+    {
+        this.name = name;
+        this.noughtOrCross = noughtOrCross;
+        this.wins = 0;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
     
+    public void setNoughtOrCross(String noughtOrCross)
+    {
+        this.noughtOrCross = noughtOrCross;
+    }
+    
+    public String getNoughtOrCross()
+    {
+        return noughtOrCross;
+    }
+    
+    public void incrementWins()
+    {
+        this.wins = this.wins + 1;
+    }
+    
+    public int getWins()
+    {
+        return wins;
+    }  
 }
